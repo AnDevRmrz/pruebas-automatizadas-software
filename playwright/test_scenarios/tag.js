1a236dc23f52f0840e36dca4dc15c022fd0273da
Hernan Dario Jimenez <hd.jimenezl1@uniandes.edu.co>
2024-11-10 11:53:23 -0500

    Playwright - E2E - Tags and Settings (#22)

--- playwright/test_scenarios/tag.js
+++ playwright/test_scenarios/tag.js
@@ -1,12 +1,16 @@
 const { SignInPage } = require("../page_objects/sign_in_page");
 const { expect } = require('@playwright/test');
 const playwright = require("playwright");
+const { Scenario } = require("../util/util");
 
-async function createTag() {
+async function createTag() {  
   
-  const browser = await playwright["chromium"].launch({ headless: false, slowMo: 500});
+  const browser = await playwright["chromium"].launch({ headless: false, slowMo: 50});
   const context = await browser.newContext();
   const page = await context.newPage();  
+  const scenario = new Scenario(page, "Create Tag");
+  scenario.begin();
+
   const email = "alguien@hotmail.com";
   const password = "123456#213asdf"
   const tagName = "Tag Name Test";
@@ -14,24 +18,95 @@ async function createTag() {
   const tagDescription = "Description Test";
 
   // Given
-  const signInPage = new SignInPage(page);
+  const signInPage = new SignInPage(scenario);
   await signInPage.goto();
   const dashboard = await signInPage.signIn(email, password);
   const listTagsPage = await dashboard.goToTags();
-  const createTagPage = await listTagsPage.goToNewTag();
+  const createEditTagPage = await listTagsPage.goToNewTag();
 
   // When
-  await createTagPage.createTag(tagName, tagSlug, tagDescription);
+  await createEditTagPage.saveTag(tagName, tagSlug, tagDescription);
 
   // Then
   await listTagsPage.goto();
   const currentTags = await listTagsPage.getListOfTags();
   expect(currentTags.some(tag => tag.name === tagName && tag.slug === tagSlug && tag.description === tagDescription)).toBeTruthy();  
   await browser.close();
+  scenario.successful();
+  return ;
+}
+
+async function editTag() {
+  
+  const browser = await playwright["chromium"].launch({ headless: false, slowMo: 50});
+  const context = await browser.newContext();
+  const page = await context.newPage();  
+  const scenario = new Scenario(page, "Edit Tag");
+  scenario.begin();
+
+  const email = "alguien@hotmail.com";
+  const password = "123456#213asdf"
+  const tagToEdit = "slug-test";
+  const tagName = "New Tag Name Test";
+  const tagSlug = "new-slug-test";
+  const tagDescription = "New Description Test";
+
+  // Given
+  const signInPage = new SignInPage(scenario);
+  await signInPage.goto();
+  const dashboard = await signInPage.signIn(email, password);
+  const listTagsPage = await dashboard.goToTags();
+  const createEditTagPage = await listTagsPage.goToEditTag(tagToEdit);
+
+  // When
+  await createEditTagPage.saveTag(tagName, tagSlug, tagDescription);
+
+  // Then
+  await listTagsPage.goto();
+  const currentTags = await listTagsPage.getListOfTags();
+  expect(currentTags.some(tag => tag.name === tagName && tag.slug === tagSlug && tag.description === tagDescription)).toBeTruthy();  
+  await browser.close();
+  scenario.successful();
+  return ;
+}
+
+async function deleteTag() {
+  
+  const browser = await playwright["chromium"].launch({ headless: false, slowMo: 50});
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const scenario = new Scenario(page, "Delete Tag");
+  scenario.begin();
+
+  const email = "alguien@hotmail.com";
+  const password = "123456#213asdf"
+  const tagToDelete = "new-slug-test";
+  const tagName = "New Tag Name Test";
+  const tagSlug = "new-slug-test";
+  const tagDescription = "New Description Test";
+
+  // Given
+  const signInPage = new SignInPage(scenario);
+  await signInPage.goto();
+  const dashboard = await signInPage.signIn(email, password);
+  let listTagsPage = await dashboard.goToTags();
+  const createEditTagPage = await listTagsPage.goToEditTag(tagToDelete);  
+
+  // When
+  listTagsPage = await createEditTagPage.deleteTag();
+
+  // Then  
+  listTagsPage = await dashboard.goToTags();
+  const currentTags = await listTagsPage.getListOfTags();
+  expect(currentTags.some(tag => tag.name === tagName && tag.slug === tagSlug && tag.description === tagDescription)).toBeFalsy();
+  await browser.close();
+  scenario.successful();
   return ;
 }
 
 module.exports = {
 
-  createTag
+  createTag,
+  editTag,
+  deleteTag
 }
