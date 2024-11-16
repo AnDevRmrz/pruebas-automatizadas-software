126225b8369408c6f8fd92407d2280cf9cb56da4
Hernan Dario Jimenez <hd.jimenezl1@uniandes.edu.co>
2024-11-16 15:29:53 -0500

    Tags and Settings - Kraken (#31)

--- base/kraken/features/017_edit_tag.feature
+++ base/kraken/features/017_edit_tag.feature
@@ -5,7 +5,7 @@ Scenario: 17 - Edit Tag
   Given I navigate to page "http://localhost:3002/ghost"
   And I wait for 1 seconds
   When I type email login "alguien@hotmail.com"  
-  And I type password "123456#213asdf"
+  And I type password login "123456#213asdf"
   And I click in sign in
   And I wait for 1 seconds
   And I click on tag main option
@@ -13,6 +13,8 @@ Scenario: 17 - Edit Tag
   And I click on the row with "slug-test" tag slug
   And I wait for 1 seconds
   And I type in the tag name field "New Tag Name Test"
+  And I wait for 1 seconds
+  And I clean the slug field
   And I type in the slug field "new-slug-test"
   And I type in the description field "New Description Test"
   And I click on the save tag button
--- base/kraken/features/web/step_definitions/step_tags.js
+++ base/kraken/features/web/step_definitions/step_tags.js
@@ -6,18 +6,23 @@ const assert = require('assert');
  */
 
 When('I click on tag main option', async function () {
-    let element = await this.driver.$('a[data-test-nav=tags]');
+    let element = await this.driver.$('a[href*="tags"]');
     return await element.click();
 });
 
 When('I click on new tag button', async function () {
-    let element = await this.driver.$("a[href$='new/']");
+    let element = await this.driver.$("a[href*='tags/new/']");
     return await element.click();
 });
 
 When('I type in the tag name field {string}', async function (tagName) {
     let element = await this.driver.$('#tag-name');
     return await element.setValue(tagName);
+});
+
+When('I clean the slug field', async function () {
+    let element = await this.driver.$('#tag-slug');
+    return await element.setValue("");
 });
 
 When('I type in the slug field {string}', async function (tagSlug) {
@@ -31,7 +36,7 @@ When('I type in the description field {string}', async function (tagDescription)
 });
 
 When('I click on the save tag button', async function () {
-    let element = await this.driver.$('button[data-test-button=save]');
+    let element = await this.driver.$('section[class=view-actions] button');
     return await element.click();
 });
 
@@ -41,54 +46,58 @@ When('I click on the row with {string} tag slug', async function (slug) {
 });
 
 When('I click on the delete tag button', async function () {
-    let element = await this.driver.$('button[data-test-button=delete-tag]');
+    let element = await this.driver.$('button[class*="gh-btn-red"]');
     return await element.click();
 });
 
 When('I confirm the deletion of the tag', async function () {
-    let element = await this.driver.$('button[data-test-button=confirm]');
+    let element = await this.driver.$('.modal-footer button[class*="gh-btn-red"]');
     return await element.click();
 });
 
 Then('I go to tags', async function() {
-    let element = await this.driver.$('a[data-test-nav=tags]');
+    let element = await this.driver.$('a[href*="tags"]');
     return await element.click();
 });
 
 Then('I can see a row with tag title name {string}', async function (tagName) {    
 
-    let titleElements = await this.driver.$$('.gh-list-row .gh-tag-list-name');
+    let titleElements = await this.driver.$$('.gh-tag-list-name');
     let titleValues = await Promise.all(titleElements.map(async (value) => await value.getText()));    
     assert.equal(titleValues.includes(tagName), true);
 });
 
 Then('I can see a row with tag description {string}', async function (tagDescription) {
 
-    let descriptionElements = await this.driver.$$('.gh-list-row .gh-tag-list-description');
+    let descriptionElements = await this.driver.$$('.gh-tag-list-description');
     let descriptionValues = await Promise.all(descriptionElements.map(async (value) => await value.getText()));    
     assert.equal(descriptionValues.includes(tagDescription), true);
 });
 
 Then('I can see a row with slug {string}', async function (slug) {    
-    let slugElements = await this.driver.$$(`.gh-list-row span[title='${slug}']`);    
-    assert.equal(slugElements.length, 1);    
+
+    let slugElements = await this.driver.$$('.gh-tag-list-slug span');
+    let slugValues = await Promise.all(slugElements.map(async (value) => await value.getText()));    
+    assert.equal(slugValues.includes(slug), true);
 });
 
 Then('I cannot see a row with tag title name {string}', async function (tagName) {    
 
-    let titleElements = await this.driver.$$('.gh-list-row .gh-tag-list-name');
+    let titleElements = await this.driver.$$('.gh-tag-list-name');
     let titleValues = await Promise.all(titleElements.map(async (value) => await value.getText()));    
     assert.equal(titleValues.includes(tagName), false);
 });
 
 Then('I cannot see a row with tag description {string}', async function (tagDescription) {
 
-    let descriptionElements = await this.driver.$$('.gh-list-row .gh-tag-list-description');
+    let descriptionElements = await this.driver.$$('.gh-tag-list-description');
     let descriptionValues = await Promise.all(descriptionElements.map(async (value) => await value.getText()));    
     assert.equal(descriptionValues.includes(tagDescription), false);
 });
 
 Then('I cannot see a row with slug {string}', async function (slug) {    
-    let slugElements = await this.driver.$$(`.gh-list-row span[title='${slug}']`);    
-    assert.equal(slugElements.length, 0);
+    
+    let slugElements = await this.driver.$$('.gh-tag-list-slug span');
+    let slugValues = await Promise.all(slugElements.map(async (value) => await value.getText()));    
+    assert.equal(slugValues.includes(slug), false);
 });
