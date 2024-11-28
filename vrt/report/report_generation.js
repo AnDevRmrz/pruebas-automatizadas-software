f09e980b2d25d29a22a28c0d71969e524cb8fae4
Andres Ramirez <ha.ramirezg1@uniandes.edu.co>
2024-11-28 15:23:56 -0500

    Ajustado reporte para generar index y comparar chromium y firefox

--- vrt/report/report_generation.js
+++ vrt/report/report_generation.js
@@ -1,35 +1,91 @@
 const { PNG } = require("pngjs");
 const pixelmatch = require("pixelmatch");
 const fs = require("fs");
+const path = require("path");
+const directoryInputBase = "./test-results";
+const directoryOutputBase = "./vrt/output";
 
-function createPixelTestCaseReport(reportObject) {
-  moveImagesToLocal(reportObject);
-  createDiffImagesPixel(reportObject);
-  const html = generateHTML(reportObject);
-  fs.writeFileSync(`${reportObject.outputResults}/report.html`, html);
+function createVRTReport() {
+  cleanOutput();
+  const directoryList = readDirectoryList(directoryInputBase);
+  for (const directory of directoryList) {
+    createTestCaseReport(directory);
+  }
+  const html = generateHTMLIndex();
+  fs.writeFileSync(
+    `${path.join(
+      directoryOutputBase,
+      "index.html"
+    )}`,
+    html
+  );
   fs.copyFileSync(
-    "./regression_tools/index-testcase.css",
-    `${reportObject.outputResults}/index.css`
+    "./vrt/report/index.css",
+    `${path.join(directoryOutputBase, 'index.css')}`
   );
 }
 
-function moveImagesToLocal(reportObject) {
-  reportObject.imagesToCompare.forEach((images) => {
-    const basePath = `${reportObject.outputResults}/base_${images.base
-      .split("/")
-      .pop()}`;
-    fs.copyFileSync(images.base, basePath);
-    images.base = basePath;
-
-    const rcPath = `${reportObject.outputResults}/rc_${images.rc
-      .split("/")
-      .pop()}`;
-    fs.copyFileSync(images.rc, rcPath);
-    images.rc = rcPath;
-  });
+function cleanOutput() {
+  if (fs.existsSync(directoryOutputBase)) {
+    fs.rmSync(directoryOutputBase, { recursive: true, force: true });
+  }
 }
 
-function createDiffImagesPixel(reportObject) {
+function createTestCaseReport(directoryPath) {
+  const reportObject = buildReportObject(directoryPath);
+  createDiffImages(reportObject);
+  const html = generateHTMLTestCase(reportObject);
+  fs.writeFileSync(
+    `${path.join(
+      directoryOutputBase,
+      reportObject.scenarioToTest,
+      "report.html"
+    )}`,
+    html
+  );
+  fs.copyFileSync(
+    "./vrt/report/index-testcase.css",
+    `${path.join(directoryOutputBase, reportObject.scenarioToTest, 'index.css')}`
+  );
+}
+
+function readDirectoryList(directoryPath) {
+  return fs
+    .readdirSync(directoryPath)
+    .map((dir) => path.join(path.resolve(directoryPath), dir));
+}
+
+function buildReportObject(directoryPath) {
+  const chromeImages = fs
+    .readdirSync(path.join(directoryPath, "chromium"))
+    .map((file) =>
+      path.join(path.resolve(path.join(directoryPath, "chromium")), file)
+    );
+  const firefoxImages = fs
+    .readdirSync(path.join(directoryPath, "firefox"))
+    .map((file) =>
+      path.join(path.resolve(path.join(directoryPath, "firefox")), file)
+    );
+
+  if (chromeImages.length !== firefoxImages.length)
+    throw new Error("Chrome and Firefox Images quantity must be the same");
+
+  imagesToCompare = [];
+  for (let i = 0; i < chromeImages.length; i++) {
+    imagesToCompare.push({
+      chrome: chromeImages[i],
+      firefox: firefoxImages[i],
+      diff: "",
+    });
+  }
+
+  return {
+    scenarioToTest: directoryPath.split("\\").pop(),
+    imagesToCompare,
+  };
+}
+
+function createDiffImages(reportObject) {
   const options = {
     threshold: 0.1,
     includeAA: true,
@@ -38,24 +94,37 @@ function createDiffImagesPixel(reportObject) {
     diffColor: [255, 0, 255],
   };
 
-  let i = 0;
+  let i = 1;
   for (const images of reportObject.imagesToCompare) {
-    const img1 = PNG.sync.read(fs.readFileSync(images.base));
-    const img2 = PNG.sync.read(fs.readFileSync(images.rc));
+    const imgChrome = PNG.sync.read(fs.readFileSync(images.chrome));
+    const imgFirefox = PNG.sync.read(fs.readFileSync(images.firefox));
 
-    const { width, height } = img1;
+    const { width, height } = imgChrome;
     const diff = new PNG({ width, height });
 
-    pixelmatch(img1.data, img2.data, diff.data, width, height, options);
-    fs.writeFileSync(
-      `${reportObject.outputResults}/diff-${i}.png`,
-      PNG.sync.write(diff)
+    pixelmatch(
+      imgChrome.data,
+      imgFirefox.data,
+      diff.data,
+      width,
+      height,
+      options
     );
-    i++;
+
+    images.diff = path.resolve(
+      `${directoryOutputBase}/${reportObject.scenarioToTest}/diff-${i++}.png`
+    );
+
+    const directory = path.dirname(images.diff);
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory, { recursive: true });
+    }
+
+    fs.writeFileSync(images.diff, PNG.sync.write(diff));
   }
 }
 
-function generateHTML(reportObject) {
+function generateHTMLTestCase(reportObject) {
   return `
     <!DOCTYPE html>
     <html lang="en">
@@ -72,7 +141,7 @@ function generateHTML(reportObject) {
           <h1 class="mb-4 text-center">${reportObject.scenarioToTest}</h1>
           <div id="comparison-cases">
             ${reportObject.imagesToCompare
-              .map((img, i) => generateBody(img, i))
+              .map((img, i) => generateBodyTestCase(img, i))
               .join("")}
           </div>
         </div>
@@ -82,38 +151,68 @@ function generateHTML(reportObject) {
     </html>`;
 }
 
-function generateBody(img, i) {
+function generateBodyTestCase(img, i) {
   i++;
   return `
     <div class="comparison-case">
       <div class="comparison-header">Paso ${i}</div>
       <div class="row">
         <div class="col-md-4 image-container">
-          <a href="${
-            img.base
-          }" data-lightbox="paso-${i}" data-title="Paso ${i}">
-            <img src="${img.base}"/>
+          <a href="${img.chrome}" data-lightbox="paso-${i}" data-title="Paso ${i}">
+            <img src="${img.chrome}"/>
           </a>
-          <div class="image-label">Imagen Base</div>
+          <div class="image-label">Chromium</div>
         </div>
         <div class="col-md-4 image-container">
-          <a href="${img.rc}" data-lightbox="paso-${i}" data-title="Paso ${i}">
-            <img src="${img.rc}"/>
+          <a href="${img.firefox}" data-lightbox="paso-${i}" data-title="Paso ${i}">
+            <img src="${img.firefox}"/>
           </a>
-          <div class="image-label">Imagen RC</div>
+          <div class="image-label">Firefox</div>
         </div>
         <div class="col-md-4 image-container">
-          <a href="diff-${
-            i - 1
-          }.png" data-lightbox="paso-${i}" data-title="Paso ${i}">
-            <img src="diff-${i - 1}.png"/>
+          <a href="${img.diff}" data-lightbox="paso-${i}" data-title="Paso ${i}">
+            <img src="${img.diff}"/>
           </a>
-          <div class="image-label">Imagen Diferencia</div>
+          <div class="image-label">Diferencia</div>
         </div>
       </div>
     </div>`;
 }
 
+function generateHTMLIndex() {
+  const directoryList = readDirectoryList(directoryOutputBase);
+  const body = directoryList
+    .map(
+      (dir) =>
+        `<a href="${path.join(
+          dir,
+          "report.html"
+        )}" class="test-case-link">${dir.split("\\").pop()}</a>`
+    )
+    .join("");
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Test Case Index</title>
+      <link href="index.css" type="text/css" rel="stylesheet">
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+    </head>
+    <body>
+      <div class="index-container">
+        <h1 class="text-center mb-4">Test Case Index</h1>
+        <p>Select a test case to view its detailed comparison:</p>
+        ${body}
+      </div>
+    </body>
+  </html>`;
+}
+
 module.exports = {
-  createPixelTestCaseReport,
+  createVRTReport,
 };
